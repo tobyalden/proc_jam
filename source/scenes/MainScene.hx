52cc7be75f131a3cb8822488238c4c934aa90718
Toby Alden <toby.alden@gmail.com>
2018-10-14 22:04:24 -0700

    Add legend for controls

--- source/scenes/MainScene.hx
+++ source/scenes/MainScene.hx
@@ -2,10 +2,23 @@ package scenes;
 
 import haxepunk.*;
 import haxepunk.graphics.*;
+import haxepunk.graphics.text.*;
 import entities.*;
 
 class MainScene extends Scene {
     override public function begin() {
         add(new Level(0, 0));
+        var text = new Text("
+R: Randomize
+A: Cellular Automata
+F: Fill All
+D: Drunken Walk
+I: Invert
+C: Clear All
+        ");
+    text.color = 0x00FF00;
+    text.smooth = false;
+    text.setBorder();
+    addGraphic(text);
     }
 }
