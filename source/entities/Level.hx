47e8f9182376cfb287fa337abbdd0465a1b5f87f
Toby Alden <toby.alden@gmail.com>
2018-10-14 19:26:58 -0700

    Add randomization

--- source/entities/Level.hx
+++ source/entities/Level.hx
@@ -6,11 +6,14 @@ import haxepunk.graphics.tile.*;
 import haxepunk.masks.*;
 
 class Level extends Entity {
+    private var grid:Grid;
+
     public function new(x:Float, y:Float) {
         super(x, y);
 
         // Create map data
-        var grid = new Grid(640, 360, 8, 8);
+        grid = new Grid(640, 360, 8, 8);
+        randomize();
         
         // Create tilemap from map data
         var tiles = new Tilemap(
@@ -22,5 +25,14 @@ class Level extends Entity {
         // Set collision mask to map data and graphic to tilemap
         mask = grid;
         graphic = tiles;
+    }
+
+    public function randomize(wallChance:Float = 0.5) {
+        // Randomize the map by setting each tile to a wall or a floor
+        for(tileX in 0...grid.columns) {
+            for(tileY in 0...grid.rows) {
+                grid.setTile(tileX, tileY, wallChance < Math.random());
+            }
+        }
     }
 }
